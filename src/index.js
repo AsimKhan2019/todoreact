c85e57c94ec610103f457e249e20f7543dc31c41
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-08-09 04:02:12 +0500

    Completed first 4 steps of the tutorial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
+import './App.css';
+import TodoContainer from './components/TodoContainer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoContainer />
+    ,
   </React.StrictMode>,
+  document.getElementById('root'),
 );
